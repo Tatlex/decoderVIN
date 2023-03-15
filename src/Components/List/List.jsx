52988f63868a22e2c4e506eb8c98670888783cae
Tatlex <51352465+Tatlex@users.noreply.github.com>
2023-03-15 14:47:41 +0200

    fifth update build for gh-pages

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,32 +1,32 @@
 import classes from "./List.module.css";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 
 function VariablesList() {
-  const [variables, setVariables] = useState([]);
+    const [variables, setVariables] = useState([]);
 
-  useEffect(() => {
-    fetch(
-      "https://vpic.nhtsa.dot.gov/api/vehicles/getvehiclevariablelist?format=json"
-    )
-      .then((res) => res.json())
-      .then((json) => setVariables(json.Results));
-  }, []);
+    useEffect(() => {
+        fetch(
+            "https://vpic.nhtsa.dot.gov/api/vehicles/getvehiclevariablelist?format=json"
+        )
+            .then((res) => res.json())
+            .then((json) => setVariables(json.Results));
+    }, []);
 
-  return (
-    <div className={classes.decoderContainer}>
-      <ul style={{ color: "#0e0e53" }}>
-        {variables.map((variable) => (
-          <li key={variable.ID}>
-            <h3>
-              <Link style={{ color: "black" }} to={`/variables/${variable.ID}`}>{variable.Name}</Link>
-            </h3>
-            <p dangerouslySetInnerHTML={{ __html: variable.Description }}></p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div className={classes.decoderContainer}>
+            <ul style={{color: "#0e0e53"}}>
+                {variables.map((variable) => (
+                    <li key={variable.ID}>
+                        <h3>
+                            <Link style={{color: "black"}} to={`/variables/${variable.ID}`}>{variable.Name}</Link>
+                        </h3>
+                        <p dangerouslySetInnerHTML={{__html: variable.Description}}></p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default VariablesList;
